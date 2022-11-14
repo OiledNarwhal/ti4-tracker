ed7368b4f296b7f0f01633e867a56d3d1fe428c7
Michael McNally <oilednarwhal@ufl.edu>
2022-11-14 13:34:29 -0600

    Added Images

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,7 +1,10 @@
 import "./GameCard.css";
-import image from ".././ti4.jpg";
+//import image from ".././ti4.jpg";
 import { useState } from "react";
 import ExpandCard from "./ExpandCard";
+import mec from "../factionImages/Mecatol Rex.png";
+import vp from "../factionImages/CustodiansVP.png";
+import date from '../Date Icon.png';
 
 /**
  *
@@ -27,6 +30,14 @@ function GameCard(props) {
   function closeModal() {
     setCardClicked(false);
   }
+  const victorIndex = props.cardInfo.players.findIndex((player) => {
+    return player.name === props.cardInfo.victor;
+  });
+  const factionCardBack = require(`../factionImages/FactionCardBacks/${props.cardInfo.players[victorIndex].faction}.png`);
+  const factionHomeSystem = require(`../factionImages/HomeSystems/${props.cardInfo.players[victorIndex].faction}.png`);
+  const factionImage = require(`../factionImages/${props.cardInfo.players[victorIndex].faction}.png`);
+
+  //<img src={factionImage} className="gamecard__faction" alt="TI4 Base" />
 
 
   return (
@@ -35,13 +46,24 @@ function GameCard(props) {
         <ExpandCard cardInfo={props.cardInfo} closeModal={closeModal} />
       )}
       <div className="gamecard" onClick={cardClickHandler}>
-        <img src={image} className="gamecard__image" alt="TI4 Base" />
-        <div className="gamecard__title">
-          <h2>{props.cardInfo.title}</h2>
-        </div>
-        <div className="gamecard__content">
-          <p>Victor: {props.cardInfo.victor}</p>
-          <p>Point Count: {props.cardInfo.pointCount}</p>
+        <img src={factionCardBack} className="gamecard__image" alt="mecatol Rex"/>
+        <div className="gamecard__positioning">
+          <div className="gamecard__title">
+            <h2>{props.cardInfo.title}</h2>
+          </div>
+          <div className="gamecard__content">
+            <div className="gamecard__victor">
+            <img src={vp} className="gamecard__victorImage" alt="An icon denoting the victor"/>
+              <div className="gamecard__victorContent">
+                <p>{props.cardInfo.victor}</p>
+                <p>{props.cardInfo.players[victorIndex].faction}</p>
+              </div>
+            </div>
+            <div className="gamecard__date">
+              <img src={date} className="gamecard__dateImage" alt="An icon denoting the date"/>
+              <p>Date: {props.cardInfo.date}</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
